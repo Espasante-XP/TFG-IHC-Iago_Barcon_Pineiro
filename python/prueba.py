6eaf61f1c32229ba4b831185a0e2424eb0fe83e2
iagobp <iago.b.pineiro@gmail.com>
2024-09-24 14:44:13 +0200

    Se han corregido algunos errores, no se continúa probando por problemas con los ficheros de las máscaras

--- python/prueba.py
+++ python/prueba.py
@@ -1,12 +1,21 @@
 
+import numpy as np
 
 from visualize_seg_mask import visualize_seg_mask
 
-imagen_mascara = 'C:/Users/iagob/OneDrive/Escritorio/Trabajo_TFG_local/TFG/Trabajo_TFG_actual/Imagenes para entrenamiento/IL6_1/Image_1034_cp_masks.png'
+imagen_mascara = 'C:/Users/iagob/OneDrive/Escritorio/Trabajo_TFG_local/TFG/TFG-IHC-Iago_Barcon_Pineiro/Imagenes_para_entrenamiento/IL6_1/Image_1031_cp_masks.png'
 
-archivo_mascara = 'C:/Users/iagob/OneDrive/Escritorio/Trabajo_TFG_local/TFG/Trabajo_TFG_actual/Imagenes para entrenamiento/IL6_1/Image_1034_seg.npy'
+archivo_mascara = '../Imagenes_para_entrenamiento/IL6_1/Image_1031_seg.npy'
 
+#C:/Users/iagob/OneDrive/Escritorio/Trabajo_TFG_local/TFG/TFG-IHC-Iago_Barcon_Pineiro/Imagenes_para_entrenamiento/IL6_1/Image_1031_seg.npy
 
-#Tengo que subir todo lo que tengo hecho al GitHub
+#C:\Users\iagob\OneDrive\Escritorio\Trabajo_TFG_local\TFG\TFG-IHC-Iago_Barcon_Pineiro\Imagenes para entrenamiento\IL6_1
 
-visualize_seg_mask(imagen_mascara, archivo_mascara)
+# Cargar el archivo .npy como un array de numpy
+mascara = np.load(archivo_mascara, allow_pickle=True) #Tengo que poner allow_pickle=True porque si no no me deja cargarlo
+
+# Imprimir la forma del array para verificar sus dimensiones
+print("máscara = ")
+print(mascara.shape)
+
+visualize_seg_mask(imagen_mascara, mascara)
